2519fdfa960421cee2c3313fd6309118390820c7
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-02-14 10:16:44 +0200

    Material model tests passed

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,7 @@
 class Material < ActiveRecord::Base
   attr_accessible :title, :description, :publication_date, :issue, :year, :pages 
   
-  
+  validates :title, :presence => true
+  validates :description, :presence => true
   
 end
