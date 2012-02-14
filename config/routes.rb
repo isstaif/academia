a5ef33e17d18c6beebd2947656fac84a1e59cd8d
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-02-14 11:16:47 +0200

    basic modifications on views and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Academia::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
