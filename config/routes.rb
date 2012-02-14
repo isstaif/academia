044d643ad8e147c19c9cbb984130c1d992c34a3d
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-02-14 11:03:35 +0200

    Pages controller generated code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Academia::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
